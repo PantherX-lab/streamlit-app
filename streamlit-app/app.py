f8dbe2f95d0f2c40be921100e2276ede103b0651
Kunle Williams <kunlewilliams@Kunles-MacBook-Pro.local>
2025-12-07 18:51:09 -0500

    Update app.py with latest code

--- streamlit-app/app.py
+++ streamlit-app/app.py
@@ -1,2 +1,11 @@
 import streamlit as st
 st.title("This is a title text!")
+
+st.title('_This_is :blue[a title] :speech_balloon:')
+
+st.title('$E = mc^2$')
+
+
+st.header('This is a header')
+
+st.subheader('This is a subheader')
